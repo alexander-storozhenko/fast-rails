972fd0055c499a0bfa03bea7fad2e23dd8e68470
storozhenko <storozhenkoalex@yandex.ru>
2021-11-01 23:01:48 +0700

    added yarn install to nodejs.sh

--- nodejs.sh
+++ nodejs.sh
@@ -2,6 +2,8 @@
 
 sudo apt-get install nodejs
 
+npm install --global yarn
+
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
--- postgres.sh
+++ postgres.sh
@@ -1,7 +1,5 @@
 # postgres
 
 sudo apt-get install postgresql postgresql-contrib libpq-dev
-cd /home
 sudo -u postgres createuser -s $USER_NAME --createdb
-cd 
 sudo systemctl restart postgresql
