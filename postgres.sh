70f61b3842f038b43c2102cabe0297d7ec92916c
storozhenko <storozhenkoalex@yandex.ru>
2021-11-03 23:53:13 +0700

    added chnaging config hba

--- postgres.sh
+++ postgres.sh
@@ -3,3 +3,5 @@
 sudo apt-get install postgresql postgresql-contrib libpq-dev
 sudo -u postgres createuser -s $USER_NAME --createdb
 sudo systemctl restart postgresql
+
+sudo python ./py/pg_hba.py $USER_NAME
--- py/pg_hba.py
+++ py/pg_hba.py
@@ -1,4 +1,27 @@
 import sys
 
-# def allow_hosts():
+user_name = sys.argv[0]
+
+config_path = '/etc/postgresql/9.5/main/pg_hba.conf'
+line = '----------------------------------'
+
+def _insert_allowed():
+  global config_path, line, user_name
+
+  with open(config_path, "r") as f:
+    file_content = f.readlines()
+  
+  index = file_content.find(line) + len(line)
+
+  file_content.insert(index, f"\n host  all     {user_name}    127.0.0.1/32      trust")
+  file_content.insert(index, f"\n host  all     {user_name}    ::1/128      trust")
+
+def allow_hosts():
+  _insert_allowed()
+
+
+if __name__ == "__main__":
+  allow_hosts()
+  
+  
   
