1a61d0c49d7bb2e1b241a70ddd944808ef02f90c
storozhenko <storozhenkoalex@yandex.ru>
2021-11-15 00:28:56 +0700

    deleted pg_gba auto

--- postgres.sh
+++ postgres.sh
@@ -3,7 +3,3 @@
 sudo apt-get install postgresql postgresql-contrib libpq-dev
 sudo -u postgres createuser -s $USER_NAME --createdb
 sudo systemctl restart postgresql
-
-sudo python ./py/pg_hba.py --usr $USER_NAME
-
-sudo systemctl restart postgresql
